aaf57f842b3944f8b8614eda7ac12ee1198c0e45
IlyassIh <ilyasihassan@gmail.com>
2025-12-12 22:44:55 +0100

    cors

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ import cors from 'cors';
 
 dotenv.config();
 connectDB();
+
 const app = express();
+
+// CORS setup
 app.use(cors({
-    origin: "*"
+    origin: 'https://ih-agency.vercel.app', // your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
 
+// Handle preflight requests for all routes
+app.options('*', cors());
+
 app.use(express.json());
+
 const PORT = process.env.PORT;
 
 app.use('/admin', routeAdmin);
